9d16b676c27a12a79cd54993841164246a262aca
IlanaFeldman <mafeldm3.ncsu.edu>
2021-10-01 10:34:54 -0400

    Test post

---
+++ CreateReadme.R
@@ -0,0 +1,5 @@
+rmarkdown::render("Project1Start.Rmd",
+                  output_format = "github_document",
+                  output_file = "readme.md",
+                  output_options = list(html_preview = FALSE)
+)
---
+++ readme.md
@@ -0,0 +1,7 @@
+Project 1 Start
+================
+Ilana Feldman
+10/1/2021
+
+This is a test to see if the Readme.md file works. It *probably*
+doesn’t.
